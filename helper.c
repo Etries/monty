10c186b8094aa258550990888ab74da6a9183cd7
Elyud <elyudmichael@gmail.com>
2023-11-16 10:36:07 +0000

    monty.c

---
+++ helper.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+/**
+ * error_exit - frees the stack and exits due to erro
+ * @stack: pointer to the head of the stack
+ *
+ */
+void error_exit(stack_t **stack)
+{
+	if (*stack)
+		free_dlistint(*stack);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * isnumber - checks if a string is a number
+ * @str: string being passed
+ *
+ * Return: returns 1 if string is a number, 0 otherwise
+ */
+int isnumber(char *str)
+{
+	unsigned int i;
+
+	if (str == NULL)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		if (str[0] == '-')
+		{
+			i++;
+			continue;
+		}
+		if (!isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
